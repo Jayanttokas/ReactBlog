22a3b5eddfb40efa9fcef0e2f1daa917a3ddc946
Jayanttokas <55356411+Jayanttokas@users.noreply.github.com>
2020-05-26 14:48:08 +0530

    Updates

---
+++ precache-manifest.b4df16aa699068a40a37fa63edbee564.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcee58e833e51bee2d4552cfef08020f",
+    "url": "/ReactBlog/index.html"
+  },
+  {
+    "revision": "1ec96502c0b9884b4cce",
+    "url": "/ReactBlog/static/js/2.ea7f39e6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/ReactBlog/static/js/2.ea7f39e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7893540f40b2babba5af",
+    "url": "/ReactBlog/static/js/main.31b25a19.chunk.js"
+  },
+  {
+    "revision": "42bb98c6b07da93540aa",
+    "url": "/ReactBlog/static/js/runtime-main.c4b2e842.js"
+  }
+]);
